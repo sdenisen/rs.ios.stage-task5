47f71183113edc1675b5eb07f01c433d47c60767
sdenisenko <sdenisen@ymail.com>
2021-06-14 23:46:01 +0300

    feat: completed.

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,6 +17,39 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+        let foodsMemoizationTable = memoizationTable(for: foods)
+        let drinksMemoizationTable = memoizationTable(for: drinks)
+        
+        var maximumDistanceToCover = 0
+        for weightOfFoodToTake in 1...maxWeight - 1 {
+            let distanceToCover = min(
+                foodsMemoizationTable[foods.count][weightOfFoodToTake],
+                drinksMemoizationTable[drinks.count][maxWeight - weightOfFoodToTake]
+            )
+            
+            if distanceToCover > maximumDistanceToCover {
+                maximumDistanceToCover = distanceToCover
+            }
+        }
+        
+        return maximumDistanceToCover
+    }
+    
+    private func memoizationTable(for suppliesPack: [Supply]) -> [[Int]] {
+        var memoizationTable = Array(repeating: Array(repeating: 0, count: maxWeight + 1), count: suppliesPack.count + 1)
+        for i in 0..<suppliesPack.count + 1 {
+            for j in 0..<maxWeight + 1 {
+                guard i != 0, j != 0 else { continue }
+                if suppliesPack[i - 1].weight > j {
+                    ///Если предмет i при ограничении веса j не помещается в принципе - то и считать нечего, нужно просто брать значение, как если бы этого предмета и не было
+                    memoizationTable[i][j] = memoizationTable[i - 1][j]
+                } else {
+                    ///memoizationTable[i - 1][j - suppliesPack[i - 1].weight -- максимальная эфффективность, которую можно получить освободив место под suppliesPack[i - 1]
+                    memoizationTable[i][j] = max(suppliesPack[i - 1].value + memoizationTable[i - 1][j - suppliesPack[i - 1].weight], memoizationTable[i - 1][j])
+                }
+            }
+        }
+        
+        return memoizationTable
     }
 }
